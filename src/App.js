ebe8c3a0fada96c920e5a5a525c97c08eda06af4
Pablo Delgadino <delgadinop@gmail.com>
2022-08-18 16:56:28 -0300

    Desafío App CLI

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Caelum Catering
+        </p>
+        <p>
+        Empresa de Catering de Cordoba, Argentina.
         </p>
         <a
           className="App-link"
